2a4066415f50e829d6f84ad22d2605a9abf2fa10
Ahmad Hasan <ahmadsalih7@hotmail.com>
2024-07-03 15:40:47 +0300

    create a router that handles the registeration process
    - create route that gets api post request and validates it.
    - hash the password using bcrypt package
    - save the data in the database.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const register = require('./routes/register');
+
+// create an app to handle API requests
+const app = express();
+app.use(express.json());
+
+// preparing listening ports
+app.listen(3000, ()=>{
+  console.log("Listening to port 3000.... ");
+});
+
+app.use('/api/register', register);
+
+
+
 mongoose.connect('mongodb://localhost:27017/blogdb')
   .then(() => console.log('MongoDB connected successfully!'))
   .catch(err => console.error('MongoDB connection error:', err));
-  
---
+++ routes/register.js
@@ -0,0 +1,37 @@
+//imports
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { User, validate } = require('../models/user');
+
+
+router = express.Router();
+router.use(express.json())
+
+// Register a new user using POST 
+router.post('/', async (req, res) => {
+    //validate the JSON input is a valid user object
+    const { error } = validate(req.body);
+    // If it's  not valid send the error and return
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // else create a a new User model instance
+    // During registration, hash the password before saving the user
+    const saltRounds = 10; // Adjust as needed for security
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+    // Create a new user with the hashed password
+    const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword
+    });
+    try {
+        res.send(await newUser.save());
+    }
+    catch(err){
+        res.status(404).send("User is already exists");
+    }
+
+});
+
+//export the Router obejct
+module.exports = router;
